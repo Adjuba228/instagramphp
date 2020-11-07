db000613c194f6bfc831bda052617f140820a969
Adjuba228 <66043680+Adjuba228@users.noreply.github.com>
2020-11-07 16:11:20 +0900

    Add files via upload

---
+++ update.php
@@ -0,0 +1,6 @@
+<?php 
+
+	$con = mysqli_connect('127.0.0.1', 'root','','instagram');
+	$query = mysqli_query($con, "UPDATE posts SET text='{$_GET['text']}',img='{$_GET['img']}' WHERE id='{$_GET['id']}'");
+	header("location:index.php");
+ ?>
